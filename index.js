5b2ddadc00258da717936565b3491fec3d849bd2
duyvo689 <vothanhduy689@gmail.com>
2021-10-30 22:34:55 +0700

    verifyToken

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend server")
--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,27 @@
 const router = require("express").Router();
-const verifyToken = require("./verifyToken")
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken")
 
 //UPDATE
-// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//     if (req.body.passWord) {
-//         req.body.passWord = CryptoJS.AES.encrypt(
-//             req.body.passWord,
-//             process.env.PASS_SEC
-//         ).toString();
-//     }
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.passWord) {
+        req.body.passWord = CryptoJS.AES.encrypt(
+            req.body.passWord,
+            process.env.PASS_SEC
+        ).toString();
+    }
 
-//     try {
-//         const updatedUser = await User.findByIdAndUpdate(
-//             req.params.id,
-//             {
-//                 $set: req.body,
-//             },
-//             { new: true }
-//         );
-//         res.status(200).json(updatedUser);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router
