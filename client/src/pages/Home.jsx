8a5eacc210fb09d51c2877f1349fd4c2e32f1e4d
duyvo689 <vothanhduy689@gmail.com>
2021-10-31 21:17:03 +0700

    products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from '../component/Navbar'
 import Slider from '../component/Slider'
 import Categories from '../component/categories'
-
+import Products from '../component/Products'
 const Home = () => {
     return (
         <div>
             < Navbar />
             <Slider />
             <Categories />
+            <Products />
         </div>
     )
 }
