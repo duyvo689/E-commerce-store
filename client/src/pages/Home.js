ac604e9941441007798cd97d283d91835b1a64ad
duyvo689 <vothanhduy689@gmail.com>
2021-10-31 20:45:14 +0700

    category

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
 import Slider from '../component/Slider'
+import Categories from '../component/categories'
 
 const Home = () => {
     return (
         <div>
             < Navbar />
             <Slider />
+            <Categories />
         </div>
     )
 }
