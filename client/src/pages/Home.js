209513c17036db5561e6cc6cfe65d0fd03045e52
duyvo689 <vothanhduy689@gmail.com>
2021-10-31 20:33:56 +0700

    slider

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
+import Slider from '../component/Slider'
 
 const Home = () => {
     return (
         <div>
             < Navbar />
+            <Slider />
         </div>
     )
 }
