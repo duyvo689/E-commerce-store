43b3bc6577f242fa88f0059b2a82159ca52042f6
duyvo689 <vothanhduy689@gmail.com>
2021-10-31 14:29:11 +0700

    init client

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+const orderRoutes = require("./routes/order");
 
 dotenv.config()
 
@@ -16,8 +18,11 @@ mongoose
     });
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/auths", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend server")
