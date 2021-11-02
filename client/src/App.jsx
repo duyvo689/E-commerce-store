cef9f5d8604ea85b997539b7c2d086f28ce3d2c9
duyvo689 <vothanhduy689@gmail.com>
2021-11-02 09:47:13 +0700

    product page + login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,38 @@
 
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
-  return <ProductList />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products">
+          <ProductList />
+        </Route>
+        <Route path="/product">
+          <Product />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -44,62 +44,63 @@ const Option = styled.option``;
 
 const ProductList = () => {
 
-    // const location = useLocation();
-    // const cat = location.pathname.split("/")[2];
-    const [filters, setFilters] = useState({});
-    const [sort, setSort] = useState("newest");
+  // const location = useLocation();
+  // const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
 
-    const handleFilters = (e) => {
-        const value = e.target.value;
-        setFilters({
-            ...filters,
-            [e.target.name]: value,
-        });
-    };
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
 
-    return (
-        <>
-            <Navbar />
-            {/* <Title>{cat}</Title> */}
-            <FilterContainer>
-                <Filter>
-                    <FilterText>Filter Products:</FilterText>
-                    <Select name="color" onChange={handleFilters}>
-                        <Option disabled>Color</Option>
-                        <Option>white</Option>
-                        <Option>black</Option>
-                        <Option>red</Option>
-                        <Option>blue</Option>
-                        <Option>yellow</Option>
-                        <Option>green</Option>
-                    </Select>
-                    <Select name="size" onChange={handleFilters}>
-                        <Option disabled>Size</Option>
-                        <Option>XS</Option>
-                        <Option>S</Option>
-                        <Option>M</Option>
-                        <Option>L</Option>
-                        <Option>XL</Option>
-                    </Select>
-                </Filter>
-                <Filter>
-                    <FilterText>Sort Products:</FilterText>
-                    <Select onChange={(e) => setSort(e.target.value)}>
-                        <Option value="newest">Newest</Option>
-                        <Option value="asc">Price (asc)</Option>
-                        <Option value="desc">Price (desc)</Option>
-                    </Select>
-                </Filter>
-            </FilterContainer>
-            {/* <Products cat={cat} filters={filters} sort={sort} /> */}
-            <Container>
-                {popularProducts.map((item) => <Product item={item} key={item.id} />)}
-            </Container>
+  return (
+    <>
+      <Navbar />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select name="color" onChange={handleFilters}>
+            <Option disabled>Color</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
+          </Select>
+          <Select name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      {/* <Products cat={cat} filters={filters} sort={sort} /> */}
+      <Container>
+        {/* {popularProducts.map((item) => <Product item={item} key={item.id} />)} */}
+      </Container>
 
-            <Newsletter />
-            <Footer />
-        </>
-    );
+      <Newsletter />
+      <Footer />
+    </>
+  );
 };
 
 export default ProductList;
